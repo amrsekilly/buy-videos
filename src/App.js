974ec59d7fb3ba9a9770d5be65d788a3c139af7b
Amr Elsekilly <github@amrsekilly.com>
2018-12-01 12:10:59 +0200

    Restructuring the project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Header from './features/app-grid/xtn-header';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <a href="#">Buy Videos</a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <Header />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/videos" component={Videos} />
+    </div>
+  </Router>
+);
+
+const Home = () => <h2>Home</h2>;
+const About = () => <h2>About</h2>;
+const Video = ({ match }) => <h3>Requested Param: {match.params.id}</h3>;
+const Videos = ({
+    match
+  }) => (
+  <div>
+    <h2>List of Videos</h2>
+
+    <ul>
+      <li>
+        <Link to={`${match.url}/video`}>Video</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/video-2`}>Video 2</Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.path}/:id`} component={Video} />
+    <Route
+      exact
+      path={match.path}
+      render={() => <h3>Please select a video.</h3>}
+    />
+  </div>
+);
 
 export default App;
